af83b49e67ae3fd8685cbf520df9e07808ec3403
鹰酱 <han_tohan@163.com>
2016-06-14 19:44:28 +0800

    lotus

--- 网易新闻01/网易新闻01/ViewController.h
+++ 网易新闻01/网易新闻01/ViewController.h
@@ -1,6 +1,6 @@
 //
 //  ViewController.h
-//  netNews
+//  网易新闻01
 //
 //  Created by 韩其 on 16/6/14.
 //  Copyright © 2016年 Compass. All rights reserved.
--- 网易新闻01/网易新闻01/main.m
+++ 网易新闻01/网易新闻01/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  netNews
+//  网易新闻01
 //
 //  Created by 韩其 on 16/6/14.
 //  Copyright © 2016年 Compass. All rights reserved.
